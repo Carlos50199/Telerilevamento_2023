bf08ecb8a2278eb86a13e904062bbb71e2dcfe41
Carlos50199 <127427222+Carlos50199@users.noreply.github.com>
2023-03-10 17:56:24 +0100

    Update R_firstcode.r

--- R_firstcode.r
+++ R_firstcode.r
@@ -3,3 +3,11 @@
 install.packages("raster")
 
 library(raster)
+
+Settiamo in R la cartella nella quale lavoreremo Working directory
+setwd("C:/Lab/")
+#Parakana Indigenous Reserve, ultimo appezzamento rimasto di una porzione più grande della Forseta Amazzonica (Prov. di Belem)
+#Dati satellitari Landsat 8, con path 224 row 63
+
+l2011<-brick("p224r63_2011_masked.grd")
+#cambiamo colore con colorRampPalette
